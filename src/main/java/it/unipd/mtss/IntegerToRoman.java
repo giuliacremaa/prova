69737f27266654a593a507061913745d176f3658
giuliacremaa <giulia.crema.2@studenti.unipd.it>
2025-05-09 10:33:34 +0200

    close #2

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -7,18 +7,15 @@ package it.unipd.mtss;
 
 public class IntegerToRoman {
     public static String convert(int number) {
-        if (number < 1 || number > 1000) {
-            throw new IllegalArgumentException("Il numero deve essere compreso tra 1 e 1000");
+        if (number < 1 || number > 3) {
+            throw new IllegalArgumentException("Non valido");
         }
 
-        // Definizione delle lettere romane e dei loro valori corrispondenti
-        String[] romanLetters = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", 
-            "X", "IX", "V", "IV", "I"};
-        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romanLetters = {"I"};
+        int[] values = {1};
 
         StringBuilder romanNumber = new StringBuilder();
 
-        // Itera attraverso i valori romani e aggiungi le lettere corrispondenti
         for (int i = 0; i < values.length; i++) {
             while (number >= values[i]) {
                 number -= values[i];
--- src/main/java/it/unipd/mtss/RomanPrinter.java
+++ src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -8,42 +8,44 @@ package it.unipd.mtss;
 public class RomanPrinter {
 
     public static String print(int num) {
+        if (num < 1 || num > 3) {
+            throw new IllegalArgumentException("Errore: il numero deve essere tra 1 e 3.");
+        }
         return printAsciiArt(IntegerToRoman.convert(num));
     }
 
     private static String printAsciiArt(String romanNumber) {
         StringBuilder asciiArt = new StringBuilder();
+    
         String[][] asciiLetters = {
-            {//Riga1
-                "  __  __ ", "  _____  ", "   _____ ", "  _      ", 
-                " __   __", " __      __", "  _____ " 
+            {
+                "  _____ " // I
             },
-            {//Riga2
-                " |  \\/  |", " |  __ \\ ", "  / ____|", " | |     ",  // L
-                " \\ \\ / /", " \\ \\    / /", " |_   _|" // I
+
+            {
+                " |_   _|" // I
             },
-            {//Riga3
-                " | \\  / |", " | |  | |", " | |     ", " | |     ",  // L
-                "  \\ V / ", "  \\ \\  / / ", "   | |  " // I
+
+            {
+                "   | |  " // I
             },
-            {//Riga4
-                " | |\\/| |", " | |  | |", " | |     ", " | |     ", // L
-                "   > <  ", "   \\ \\/ /  ", "   | |  " // I
-            }, 
-            {//Riga5
-                " | |  | |", " | |__| |", " | |____ ", " | |____ ", // L
-                "  / . \\ ", "    \\  /   ", "  _| |_ " // I
-            }, 
-            {//Riga6
-                " |_|  |_|", " |_____/ ", "  \\_____|", " |______|", // L
-                " /_/ \\_\\", "     \\/    ", " |_____|" // I
+
+            {
+                "   | |  " // I
+            },
+                
+            {
+                "  _| |_ " // I
+            },
+                
+            {
+                " |_____|" // I
             },
         };
         
-        for( int z=0; z<6; z++) {
+        for(int z=0; z<6; z++) {
             for (char c : romanNumber.toCharArray()) {
-                // Aggiungi la rappresentazione ASCII art orizzontale della lettera corrente
-                asciiArt.append(asciiLetters[z]["MDCLXVI".indexOf(c)]);
+                asciiArt.append(asciiLetters[z]["I".indexOf(c)]);
             }
             asciiArt.append("\n");
         }
